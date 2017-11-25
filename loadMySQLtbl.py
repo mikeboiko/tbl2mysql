ddde9a564dc56acac9672242df0cc2facabd6e90
Mike Boiko <michaelboiko@gmail.com>
2017-11-25 09:08:54 -0700

    CSV/Excel files can both be loaded into DB - script knows based on file extension

--- loadMySQLtbl.py
+++ loadMySQLtbl.py
@@ -8,8 +8,10 @@
 # Imports {{{1
 
 import argparse
-import os, sys
+import csv
+import os
 import pymysql
+import sys
 import xlrd
 
 # Parse Arguments {{{1
@@ -42,7 +44,7 @@ parser.add_argument('-t', '--table', dest='sqlTableName', action='store',
 
 args = parser.parse_args()
 
-# Variables {{{1
+# Variables/Constants {{{1
 
 # File extension string lists
 fileExtensionsCSV = []
@@ -62,23 +64,120 @@ def initFileExtensionLists(): # {{{2
     fileExtensionsExcel.append("xls")
 
 def determineInputTableType(): # {{{2
-    "Look at file extension to determine table type"
+    'Look at file extension to determine table type'
 
     # Declare global namespace variables
     global inputTableIsExcel
     global inputTableIsCSV
 
+    # Initialize values
+    inputTableIsExcel = False
+    inputTableIsCSV = False
+
     # Check for the the input table file's extension
     # The . is added before extensiong and the string is made lower case
+    # Excel Table
     if any('.'+substring in args.inputTableName.lower() for substring in fileExtensionsExcel):
         inputTableIsExcel = True
-        inputTableIsCSV = False
+
+    # CSV Table
     elif any('.'+substring in args.inputTableName.lower() for substring in fileExtensionsCSV):
-        inputTableIsExcel = False
         inputTableIsCSV = True
+
+    # Unknown file type
     else:
         sys.exit('Error: Unrecognizable File Extension for {}'.format(args.inputTableName))
 
+def defineSqlDBTableName(): # {{{2
+    'MySQL db Table Name'
+
+    global sqlTableName
+
+    # Defautlt table name - same as the input file name
+    if args.sqlTableName == "default":
+        # Strip out file name from path
+        sqlTableName = os.path.basename(args.inputTableName)
+        sqlTableName = os.path.splitext(sqlTableName)[0]
+        # Only keep alpha-numeric characters
+        sqlTableName = ''.join(e for e in sqlTableName if e.isalnum())
+
+    # User defined table name in argument
+    else:
+        sqlTableName = args.sqlTableName
+
+def initializeCSV(): # {{{2
+    'Perform initialization for CSV file types'
+    print('Parsing {} CSV table into {} MySql db'.format(args.inputTableName, args.database))
+
+    global headerRow # Table field names
+
+    with open(args.inputTableName, newline='') as fileCSV:
+        reader = csv.reader(fileCSV)
+        headerRow = next(reader)  # gets the first line
+
+def initializeExcel(): # {{{2
+    'Perform initialization for Excel file types'
+    print('Parsing {} Excel table into {} MySql db'.format(args.inputTableName, args.database))
+
+    global headerRow # Table field names
+    global sheet     # Excel worksheet object
+
+    excelWorkbookName = xlrd.open_workbook(args.inputTableName)
+    if args.excelWorksheetName == 'default':
+        sheet = excelWorkbookName.sheet_by_index(0)
+    else:
+        sheet = excelWorkbookName.sheet_by_name(args.excelWorksheetName)
+    headerRow = sheet.row_values(0)
+
+def prepareSqlQueries(): # {{{2
+    'Prepare sql query strings'
+
+    global sqlQueryCreate # Create Table
+    global sqlQueryInsert # Insert into Table
+
+    sqlQueryCreate = "create table " + sqlTableName + " (id int not null auto_increment primary key, "
+    sql2a = "insert into " + sqlTableName + " ("
+    sql2b = ""
+    for header in headerRow:
+        sqlQueryCreate += header + " text,\n"
+        sql2a += header + ", "
+        sql2b += "%s, "
+    sqlQueryCreate = sqlQueryCreate[:-2] # Remove last ,
+    sqlQueryCreate += ")"
+    sql2a = sql2a[:-2] # Remove last ,
+    sql2b = sql2b[:-2] # Remove last ,
+    sqlQueryInsert = sql2a + ") VALUES (" + sql2b + ")"
+
+def sqlCreateTable(): # {{{2
+    'Create Table in MySQL db'
+
+    # Delete old table
+    cursor.execute("drop table if exists " + sqlTableName)
+
+    # Create new table
+    cursor.execute(sqlQueryCreate)
+
+
+def sqlInsertDataFromCSV(): # {{{2
+    'Insert data from CSV table into SQL table'
+
+    with open(args.inputTableName, newline='') as fileCSV:
+        reader = csv.reader(fileCSV)
+        next(reader) # Skip header line
+        for row in reader:
+            values = () # blank tuple
+            for cell in row:
+                values = values + (cell,)
+            cursor.execute(sqlQueryInsert, values)
+
+def sqlInsertDataFromExcel(): # {{{2
+    'Insert data from Excel table into SQL table'
+
+    for rowNum in range(1, sheet.nrows):
+        values = () # blank tuple
+        for colNum in range(0, sheet.ncols):
+            values = values + (sheet.cell(rowNum,colNum).value,)
+        cursor.execute(sqlQueryInsert, values)
 
 # Main Program {{{1
 
@@ -88,26 +187,25 @@ initFileExtensionLists()
 # Look at file extension to determine table type
 determineInputTableType()
 
-print(args.inputTableName)
-# print('Excel Flag is {}, CSV flag is {}'.format(inputTableIsExcel, inputTableIsCSV))
-# os.system("pause")
+# MySQL db Table Name
+defineSqlDBTableName()
 
+# Initialize CSV file types
 if inputTableIsCSV:
-    print("CSV")
-else:
-    print("Excel")
+    initializeCSV()
+# Initialize Excel file types
+elif inputTableIsExcel:
+    initializeExcel()
+# There may be other types of tables other than Excel/CSV added later
 
-os.system("pause")
-sys.exit()
+# Prepare sql query strings
+prepareSqlQueries()
 
-# Excel Connection {{{2
-excelWorkbookName = xlrd.open_workbook(args.inputTableName)
-if args.excelWorksheetName == 'default':
-    sheet = excelWorkbookName.sheet_by_index(0)
-else:
-    sheet = excelWorkbookName.sheet_by_name(args.excelWorksheetName)
+# It made more sense to keep the table creation/insertion queries seperate
+# This way the insert query doesn't have to store all data in memory
+# but rather reads one row at a time, inserts and moves on
 
-# MySQL Connection {{{2
+# MySQL Connection
 db = pymysql.connect(host=args.host,
                      port=args.port,
                      user=args.user,
@@ -115,44 +213,21 @@ db = pymysql.connect(host=args.host,
                      db=args.database)
 cursor = db.cursor()
 
-# MySQL Table Name {{{2
-if args.sqlTableName == "default":
-    sqlTableName = args.inputTableName.replace('.xlsx','').replace('.xlsm','').replace('.xls','')
-    # Strip special characters from Excel file name
-    sqlTableName = ''.join(e for e in sqlTableName if e.isalnum())
-else:
-    sqlTableName = args.sqlTableName
+# Create Table in MySQL db
+sqlCreateTable()
 
-# Prepare Queries. {{{2
-# sql1: create table, sql2: insert
-excelHeaders = sheet.row_values(0)
-sql1 = "create table " + sqlTableName + " (id int not null auto_increment primary key, "
-sql2a = "insert into " + sqlTableName + " ("
-sql2b = ""
-for header in excelHeaders:
-    sql1 += header + " text,\n"
-    sql2a += header + ", "
-    sql2b += "%s, "
-sql1 = sql1[:-2] # Remove last ,
-sql1 += ")"
-sql2a = sql2a[:-2] # Remove last ,
-sql2b = sql2b[:-2] # Remove last ,
-sql2 = sql2a + ") VALUES (" + sql2b + ")"
+# Insert data from CSV table into SQL table
+if inputTableIsCSV:
+    sqlInsertDataFromCSV()
+# Insert data from Excel table into SQL table
+elif inputTableIsExcel:
+    sqlInsertDataFromExcel()
+# There may be other types of tables other than Excel/CSV added later
 
-# Modify Table {{{2
-
-# Create Table
-cursor.execute("drop table if exists " + sqlTableName)
-cursor.execute(sql1)
-
-# Insert all data into table
-for rownum in range(1, sheet.nrows):
-    values = () # blank tuple
-    for colnum in range(0, sheet.ncols):
-        values = values + (sheet.cell(rownum,colnum).value,)
-    cursor.execute(sql2, values)
-
-# Close out {{{2
+# Close DB Connection {{{2
 cursor.close()
 db.commit()
 db.close()
+
+# os.system("pause")
+# sys.exit()
